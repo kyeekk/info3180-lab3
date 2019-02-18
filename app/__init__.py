825f199494ae1bf0e509f8e4a46dd3865dbedbf3
Kayla Effs <kaylaeffs@gmail.com>
2019-02-18 00:53:15 +0000

    form class creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask_mail import Mail
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'enter some random passphrase'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '2525' 
+app.config['MAIL_USERNAME'] = 'f5dab85f6a961b'
+app.config['MAIL_PASSWORD'] = '212875bf55a172'
+
+mail = Mail(app)
 from app import views
---
+++ app/forms.py
@@ -0,0 +1,12 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class form(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
+    
