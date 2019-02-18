37e73dc843e885e8ece945eee2b75c1cbd43b1a3
Kayla Effs <kaylaeffs@gmail.com>
2019-02-18 02:05:15 +0000

    form & message creation

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
 
-class form(FlaskForm):
+class Form(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     subject = StringField('Subject', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -7,7 +7,14 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app import mail
+from flask_mail import Message
+from flask_wtf.csrf import CSRFProtect 
 
+
+### CSRF Protection
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 ###
 # Routing for your application.
@@ -18,11 +25,30 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+
+
+@app.route('/contact', methods=('GET', 'POST'))
+def contact():
+    form = Form()
+    if request.method == 'POST':
+        if form.validate_on_submit() == True :
+            name = form.name.data
+            email = form.email.data
+            subject = form.subject.data
+            message = form.message.data
+            msg = Message(subject, sender=(name, email),recipients=["kaylaeffs@outlook.com"])
+            msg.body = message
+            mail.send(msg)
+            flash('Successful.')
+            return redirect(url_for('home'))
+        
+    """Renders the contact form"""
+    return render_template('contact.html', form = form)
 
 
 ###
